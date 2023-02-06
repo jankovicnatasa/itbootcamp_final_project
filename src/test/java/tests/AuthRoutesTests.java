3893132f63b12b1aa3c7a1445b2396dec07242bf
Natasa <natasajankovic1995@gmail.com>
2023-02-06 06:37:44 +0100

    AuthRoutes Tests Added

--- src/test/java/tests/AuthRoutesTests.java
+++ src/test/java/tests/AuthRoutesTests.java
@@ -1,4 +1,45 @@
 package tests;
 
-public class AuthRoutesTests {
+import org.openqa.selenium.By;
+import org.testng.annotations.*;
+
+
+public class AuthRoutesTests extends BaseTest {
+
+    @BeforeMethod
+    @Override
+    public void beforeMethod() {
+        super.beforeMethod();
+        if (driver.getCurrentUrl().equals("https://vue-demo.daniel-avellaneda.com/home")) {
+            driver.findElement(By.linkText("Logout")).click();
+        }
+    }
+
+    @Test
+    public void forbidsHomeUrl() {
+        driver.get("https://vue-demo.daniel-avellaneda.com/home");
+        String currentUrl = driver.getCurrentUrl();
+        assert currentUrl.contains("/login");
+    }
+
+    @Test
+    public void forbidsProfileUrl() {
+        driver.get("https://vue-demo.daniel-avellaneda.com/profile");
+        String currentUrl = driver.getCurrentUrl();
+        assert currentUrl.contains("/login");
+    }
+
+    @Test
+    public void forbidsAdminCitiesUrl() {
+        driver.get("https://vue-demo.daniel-avellaneda.com/admin/cities");
+        String currentUrl = driver.getCurrentUrl();
+        assert currentUrl.contains("/login");
+    }
+
+    @Test
+    public void forbidsAdminUsersUrl() {
+        driver.get("https://vue-demo.daniel-avellaneda.com/admin/users");
+        String currentUrl = driver.getCurrentUrl();
+        assert currentUrl.contains("/login");
+    }
 }
