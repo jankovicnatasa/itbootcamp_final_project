a88b3aad007c2cbda66a9ef2ebf7c0a4d98d7b71
Natasa <natasajankovic1995@gmail.com>
2023-02-06 06:00:26 +0100

    SignUp Tests Added

--- src/test/java/pages/SignUpPage.java
+++ src/test/java/pages/SignUpPage.java
@@ -5,8 +5,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import javax.xml.ws.wsaddressing.W3CEndpointReference;
-
 public class SignUpPage extends BasePage {
 
     @FindBy(id = "name")
@@ -21,10 +19,60 @@ public class SignUpPage extends BasePage {
     @FindBy(id = "confirmPassword")
     private WebElement confirmPasswordField;
 
-    @FindBy(className = "v-btn__content")
+    @FindBy(xpath = "//*[@id=\"app\"]/div[1]/main/div/div[2]/div/div/div[2]/span/form/div/div[5]/button")
     private WebElement signMeUpButton;
+
+    @FindBy(xpath = "//*[@id=\"app\"]/div[1]/main/div/div[2]/div/div/div[3]/div/div/div/div/div[1]/ul/li")
+    private WebElement errorMessage;
+
+    @FindBy (className = "dlgVerifyAccount")
+    private WebElement importantMessage;
+
+    @FindBy (className = "btnClose")
+    private WebElement closeMessageButton;
 
     public SignUpPage(WebDriver driver, WebDriverWait driverWait) {
         super(driver, driverWait);
     }
+
+    public WebElement getEmailField() {
+        return emailField;
+    }
+
+    public WebElement getPasswordField() {
+        return passwordField;
+    }
+
+    public WebElement getConfirmPasswordField() {
+        return confirmPasswordField;
+    }
+
+    public WebElement getErrorMessage() {
+        return errorMessage;
+    }
+
+    public WebElement getImportantMessage() {
+        return importantMessage;
+    }
+
+    public WebElement getCloseMessageButton() {
+        return closeMessageButton;
+    }
+
+    public void signUp(String name, String email, String password, String confirmPassword) {
+        this.nameField.clear();
+        this.nameField.sendKeys(name);
+
+        this.emailField.clear();
+        this.emailField.sendKeys(email);
+
+        this.passwordField.clear();
+        this.passwordField.sendKeys(password);
+
+        this.confirmPasswordField.clear();
+        this.confirmPasswordField.sendKeys(confirmPassword);
+
+        signMeUpButton.click();
+    }
+
 }
--- src/test/java/tests/SignupTests.java
+++ src/test/java/tests/SignupTests.java
@@ -1,4 +1,81 @@
 package tests;
 
-public class SignupTests {
+import com.github.javafaker.Faker;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.List;
+
+public class SignupTests extends BaseTest {
+
+    Faker faker = new Faker();
+
+    @BeforeMethod
+    @Override
+    public void beforeMethod() {
+        super.beforeMethod();
+        homePage.openSignupPage();
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        List<WebElement> logoutButton = driver.findElements(By.xpath("//*[@id=\"app\"]/div/div/header/div/div[3]/button[1]"));
+        if (!logoutButton.isEmpty()) {
+            logoutButton.get(0).click();
+        }
+    }
+
+    @Test
+    public void visitSignupPage() {
+        String actualUrl = driver.getCurrentUrl();
+        Assert.assertTrue(actualUrl.contains("/signup"));
+    }
+
+    @Test
+    public void checkInputTypes() {
+        String emailType = signUpPage.getEmailField().getAttribute("type");
+        Assert.assertEquals(emailType, "email");
+
+        String passwordType = signUpPage.getPasswordField().getAttribute("type");
+        Assert.assertEquals(passwordType, "password");
+
+        String confirmPasswordType = signUpPage.getConfirmPasswordField().getAttribute("type");
+        Assert.assertEquals(confirmPasswordType, "password");
+    }
+
+    @Test
+    public void userAlreadyExistsErrors() {
+        signUpPage.signUp("Test Test", "admin@admin.com", "123654", "123654");
+
+        driverWait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
+        String expectedMessage = "E-mail already exists";
+        String actualMessage = signUpPage.getErrorMessage().getText();
+        Assert.assertEquals(actualMessage, expectedMessage);
+
+        visitSignupPage();
+    }
+
+    @Test
+    public void signup() {
+        signUpPage.signUp("Natasa Jankovic", faker.internet().emailAddress(), "12345", "12345");
+        String expectedMessage = "IMPORTANT: Verify your account";
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        String actualMessage = signUpPage.getImportantMessage().getText();
+        Assert.assertEquals(actualMessage, expectedMessage);
+
+        signUpPage.getCloseMessageButton().click();
+    }
 }
